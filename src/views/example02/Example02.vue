ebe5eb51bd0da16f60795dd9f75b7756974632ce
Eric Gao <754095810@qq.com>
2020-04-22 15:19:15 +0800

    computed02

--- src/views/example02/Example02.vue
+++ src/views/example02/Example02.vue
@@ -5,13 +5,15 @@
     <p>{{ isShow ? "为true显示" : "为false显示" }}</p>
     <p>{{ msg }}</p>
     <!-- 模板上不能声明this -->
-    <p>{{ user.name }} / {{ replace }}</p>
+    <p>{{ user.name }} / {{ myReplace(user.insertTime) }}</p>
     <button @click="changed">change name</button>
+    <p>地址：{{ user.address }}</p>
+    <button @click="addAddress">add address</button>
   </div>
 </template>
 
 <script>
-// 对外暴露vue对象，默认继承自vuex
+// 对外暴露vue对象，默认继承自vue
 export default {
   // 小括号加大括号直接就是返回的结果
   // 属性是一个函数
@@ -20,13 +22,17 @@ export default {
     msg: "afa",
     user: {
       name: "bo",
-      insertTime: "2020T08"
+      insertTime: "2020T08",
+      address: null
     }
   }),
   // 属性是一个方法
   methods: {
     changed() {
       this.user.name = "gao";
+    },
+    addAddress() {
+      this.user.address = "956";
     }
   },
   // 属性是一个方法
@@ -34,6 +40,10 @@ export default {
     // 获取属性计算的结果，replace是一个属性，只是属性getter方法的简写
     replace() {
       return this.user.insertTime.replace("T", " ");
+    },
+    myReplace() {
+      // 返回值是一个函数
+      return date => date.replace("T", " ");
     }
   }
 };
